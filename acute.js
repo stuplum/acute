0a81c125f0da9b90ee0a6d0d405ee0b3aabb0c92
stuplum <stuart.plumbley@bskyb.com>
2013-10-21 13:30:47 +0100

    packaged 0.0.5

--- acute.js
+++ acute.js
@@ -1,6 +1,6 @@
 (function () {
 
-/*! acute - v0.0.4 - 2013-10-20
+/*! acute - v0.0.5 - 2013-10-21
 * Copyright (c) 2013 stuplum <stuplum@gmail.com>; Licensed  */
 
 'use strict';
@@ -18,13 +18,13 @@ angular.module('acute.gravatar', ['acute.md5'])
         .factory('gravatarService', ['md5', function (md5) {
 
             return {
-                getImageSrc: function (email, attrs) {
+                getImageSrc: function (gravatarEmail, attrs) {
 
-                    var hash       = md5.createHash(email.toLowerCase()),
-                        domain     = attrs.secure ? 'https://secure' : 'http://www',
-                        size       = attrs.size || 40,
-                        rating     = attrs.rating || 'pg',
-                        defaultUrl = attrs.default || '404';
+                    var hash       = md5.createHash(gravatarEmail.toLowerCase()),
+                        domain     = attrs.gravatarSecure ? 'https://secure' : 'http://www',
+                        size       = attrs.gravatarSize || 40,
+                        rating     = attrs.gravatarRating || 'pg',
+                        defaultUrl = attrs.gravatarDefault || '404';
 
                     return domain + '.gravatar.com/avatar/' + hash + '?s=' + size + '&r=' + rating + '&d=' + defaultUrl;
                 }
@@ -39,14 +39,14 @@ angular.module('acute.gravatar', ['acute.md5'])
 
                 link: function (scope, el, attrs) {
 
-                    scope.$watch(attrs.email, function (email) {
+                    scope.$watch(attrs.gravatarEmail, function (gravatarEmail) {
 
-                        if(email) {
-                            var emailDefined = (email !== null) && (email !== undefined) && (email !== ''),
-                                emailValid   = (null != email.match(/.*@.*\..{2}/));
+                        if(gravatarEmail) {
+                            var emailDefined = (gravatarEmail !== null) && (gravatarEmail !== undefined) && (gravatarEmail !== ''),
+                                emailValid   = (null != gravatarEmail.match(/.*@.*\..{2}/));
 
                             if (emailDefined && emailValid) {
-                                el.attr('src', gravatarService.getImageSrc(email, attrs));
+                                el.attr('src', gravatarService.getImageSrc(gravatarEmail, attrs));
                             }
                         }
 
